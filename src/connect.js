e575a14b9521853533980651160fa5a075d258bc
Cauê Alves <cauealvesb4@gmail.com>
2024-09-08 08:49:44 -0300

    fix: Alteração na conexão do banco

--- src/connect.js
+++ src/connect.js
@@ -4,11 +4,10 @@ import express from 'express';
 dotenv.config()
 
 const port = process.env.PORT
-const MongoDb = process.env.MONGODB_URL
 const app = express();
 
 //Connected Port and DataBase
-const dataBase = mongoose.connect(MongoDb).then(() => {
+const dataBase = mongoose.connect(`mongodb+srv://${process.env.NAME_USER}:${process.env.PASSWORD}@users.kpfly.mongodb.net/?retryWrites=true&w=majority&appName=Users`).then(() => {
     console.log('Database connected!');
     app.listen(port, () => {
         console.log('Port access successfully')
