311cc3ad48b5e29b690a28a468c473f97ef34140
Cauê Alves <cauealvesb4@gmail.com>
2024-09-19 17:55:48 -0300

    feat: Criação de uma rota na parte do 'Register' para voltar para a home

--- src/routes.js
+++ src/routes.js
@@ -131,8 +131,26 @@ app.post('/register', register, async (req, res) => {
         }
         const user = await UserModel.create(newUser)
         res.status(201).send(`
-            <div style="display: flex; justify-content: center; align-items: center; height: 100vh;">
+            <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
                 <h1>Bem-vindo ${req.body.username}</h1>
+                <a href="/home" style="text-decoration: none; margin-top: 20px;">
+                    <button style="
+                        padding: 10px 20px; 
+                        font-weight: bold; 
+                        color: white; 
+                        border-radius: 2rem; 
+                        cursor: pointer; 
+                        width: 200px; 
+                        height: 50px; 
+                        border: none; 
+                        background-color: #4F46E5; 
+                        display: flex; 
+                        justify-content: center; 
+                        align-items: center; 
+                        transition: background-color 0.3s;">
+                        Voltar
+                    </button>
+                </a>
             </div>
         `); 
 
@@ -180,7 +198,7 @@ app.get('/delete', middlewareJWT, async (req, res) => {
         res.status(200).send(`
         <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
         <h1>Deletado com Sucesso</h1>
-        <a href="/home" style="text-decoration: none;"> <!-- Usei um link em torno do botão -->
+        <a href="/home" style="text-decoration: none;">
             <button style="
                 margin-top: 20px; 
                 padding: 10px 20px; 
@@ -205,11 +223,6 @@ app.get('/delete', middlewareJWT, async (req, res) => {
         res.status(400).send(error.message)
     } 
 });
-
-app.get('/views', (req, res) => {
-    res.render("pagesDelete")
-});
-
 
 app.get('/login', (req, res) => {
     res.render("pagesLogin")
