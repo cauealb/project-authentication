e1fcbb3169cedb9b550edb8eb81541512dfa173b
Cauê Alves <cauealvesb4@gmail.com>
2024-09-20 06:12:57 -0300

    fix: Validando ainda mais o username e password

--- src/routes.js
+++ src/routes.js
@@ -46,11 +46,37 @@ const middlewareJWT  = async (req, res, next) => {
 const register = async (req, res, next) => {
     const {username, password} = req.body
     try {
-    const findUser = await UserModel.findOne({username: username    })
-    if(findUser){
+        if(username.length < 6 || password.length < 6){
+            return res.status(400).send(`
+                <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
+                    <h1>Username ou Password precisam de no mínimo 7 digitos!</h1>
+                    <a href="/register" style="text-decoration: none; margin-top: 20px;">
+                        <button style="
+                            padding: 10px 20px; 
+                            font-weight: bold; 
+                            color: white; 
+                            border-radius: 2rem; 
+                            cursor: pointer; 
+                            width: 200px; 
+                            height: 50px; 
+                            border: none; 
+                            background-color: #4F46E5; 
+                            display: flex; 
+                            justify-content: center; 
+                            align-items: center; 
+                            transition: background-color 0.3s;">
+                            Voltar
+                        </button>
+                    </a>
+                </div>
+            `);
+        }
+        
+    const findUser = await UserModel.findOne({username: username})
+    if(findUser || username.length < 6){
         return res.status(400).send(`
             <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
-                <h1>Username já cadastrado no nosso banco de dados!</h1>
+                <h1>Username inválido!</h1>
                 <a href="/register" style="text-decoration: none; margin-top: 20px;">
                     <button style="
                         padding: 10px 20px; 
