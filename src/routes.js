9b812f24c715f80e4b57d222920f2a7f9b183240
Cauê Alves <cauealvesb4@gmail.com>
2024-09-22 12:56:06 -0300

    feat: Melhorando a validação do 'register'

--- src/routes.js
+++ src/routes.js
@@ -124,6 +124,37 @@ const register = async (req, res, next) => {
             </div>
         `);
     }
+    const splUser = username.split(" ")
+    const splPass = password.split(" ")
+
+    if(splUser.length === 2 || splPass.length === 2){
+        return res
+        .status(400)
+        .send(`
+            <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
+                <h1>Username ou Password precisam de 1 palavra!!</h1>
+                <a href="/register" style="text-decoration: none; margin-top: 20px;">
+                    <button style="
+                        padding: 10px 20px; 
+                        font-weight: bold; 
+                        color: white; 
+                        border-radius: 2rem; 
+                        cursor: pointer; 
+                        width: 200px; 
+                        height: 50px; 
+                        border: none; 
+                        background-color: #4F46E5; 
+                        display: flex; 
+                        justify-content: center; 
+                        align-items: center; 
+                        transition: background-color 0.3s;">
+                        Voltar
+                    </button>
+                </a>
+            </div>
+        `)
+    }
+
     next();
     } catch (error) {
         res.status(410).send(error.message)
