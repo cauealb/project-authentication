aaf3bed1eea23e86f42489bf1b0b44c11c431c16
Cauê Alves <cauealvesb4@gmail.com>
2024-09-24 18:40:19 -0300

    feat: Criação do router para rota de 'login'

--- src/routes.js
+++ src/routes.js
@@ -20,7 +20,11 @@ app.use(session({
 app.set("view engine", "ejs")
 app.set("views", "src/views")
 
+//Register
 import registerRouter from './Routes/register.js'
+
+//Login
+import loginRouter from './Routes/login.js'
 
 
 //MiddlewaresJWT
@@ -44,69 +48,6 @@ const middlewareJWT  = async (req, res, next) => {
         })
     }
 };
-
-//Middleware Login
-const login = async (req, res, next) => {
-    const {username, password} = req.body
-    const findUser = await UserModel.findOne({username})
-    if(!findUser){
-        return res
-        .status(400)
-        .send(`
-            <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
-                <h1>Username não foi encontrado!</h1>
-                <a href="/login" style="text-decoration: none; margin-top: 20px;">
-                    <button style="
-                        padding: 10px 20px; 
-                        font-weight: bold; 
-                        color: white; 
-                        border-radius: 2rem; 
-                        cursor: pointer; 
-                        width: 200px; 
-                        height: 50px; 
-                        border: none; 
-                        background-color: #4F46E5; 
-                        display: flex; 
-                        justify-content: center; 
-                        align-items: center; 
-                        transition: background-color 0.3s;">
-                        Voltar
-                    </button>
-                </a>
-            </div>
-        `)
-    }
-    
-    const findPassword = await bcrypt.compare(password, findUser.password)
-    if(!findPassword){
-        return res
-        .status(400)
-        .send(`
-            <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
-                <h1>Senha inválida</h1>
-                <a href="/login" style="text-decoration: none; margin-top: 20px;">
-                    <button style="
-                        padding: 10px 20px; 
-                        font-weight: bold; 
-                        color: white; 
-                        border-radius: 2rem; 
-                        cursor: pointer; 
-                        width: 200px; 
-                        height: 50px; 
-                        border: none; 
-                        background-color: #4F46E5; 
-                        display: flex; 
-                        justify-content: center; 
-                        align-items: center; 
-                        transition: background-color 0.3s;">
-                        Voltar
-                    </button>
-                </a>
-            </div>
-        `)
-    }
-    next();
-}
 
 //Middleware de Update
 const update = async (req, res, next) =>  {
@@ -298,22 +239,6 @@ app.post('/admin', middlewareJWT, isAdmin, async (req, res) => {
 }
 })
 
-app.post('/login', login, async (req, res) => {
-    const userPL = {
-        username: req.body.username,
-        password: req.body.password
-    };
-    const acessToken = jwt.sign(userPL, process.env.SECRET)
-    req.session.jwt = acessToken
-
-    //Colocando o token no HTML
-    const data = {
-        username: userPL.username,
-        token: req.session.jwt
-    }
-    res.status(200).render(`pagesLoginHome`, data) 
-});
-
 app.post('/atualizar', middlewareJWT, update, async(req, res) => {
     const user = {
         username: req.body.username,
@@ -453,6 +378,8 @@ app.post('/delete', middlewareJWT, isDeleteValid, async (req, res) => {
 app.get('/login', (req, res) => {
     res.render("pagesLogin")
 });
+
+app.use(loginRouter)
 
 app.get('/register', (req, res) => {
     res.render("pagesRegister")
