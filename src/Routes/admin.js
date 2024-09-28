56b86bce0ef7f8e3248453ebf11c9a831277546b
Cauê Alves <cauealvesb4@gmail.com>
2024-09-28 08:56:50 -0300

    feat: Melhorando a segurnaça da rota de 'admin'

--- src/Routes/admin.js
+++ src/Routes/admin.js
@@ -6,6 +6,46 @@ import dotenv from 'dotenv'
 dotenv.config()
 import UserModel from "../models/modeluser.js";
 import { middlewareJWT } from "./middlewareJWT.js";
+import jwt from 'jsonwebtoken'
+
+//Middleware para certificar se é um admin
+const tokenAdmin = (req, res, next) => {
+    try {
+    const token = jwt.decode(req.session.jwt, process.env.SECRET)
+    const {username, password} = token
+
+    if(username !== process.env.ADMIN_USER || password !== process.env.ADMIN_PASSWORD){
+        return res
+        .status(400)
+        .send(`
+            <div style="display: flex; flex-direction: column; justify-content: center; align-items: center; height: 100vh;">
+                <h1>Rota não autorizada para o seu tipo de usuário!</h1>
+                <a href="/admin" style="text-decoration: none; margin-top: 20px;">
+                    <button style="
+                        padding: 10px 20px; 
+                        font-weight: bold; 
+                        color: white; 
+                        border-radius: 2rem; 
+                        cursor: pointer; 
+                        width: 200px; 
+                        height: 50px; 
+                        border: none; 
+                        background-color: #4F46E5; 
+                        display: flex; 
+                        justify-content: center; 
+                        align-items: center; 
+                        transition: background-color 0.3s;">
+                        Voltar
+                    </button>
+                </a>
+            </div>    
+        `)
+    }
+    next();
+    } catch (error) {
+        res.status(403).send(error.message)
+    }
+}
 
 //Middleware de Admin
 const isAdmin = (req, res, next) => {
@@ -80,7 +120,7 @@ const isAdmin = (req, res, next) => {
     
 }
 
-router.post('/admin', middlewareJWT, isAdmin, async (req, res) => {
+router.post('/admin', middlewareJWT, tokenAdmin, isAdmin, async (req, res) => {
     try {
         const user = await UserModel.find({})
         res.status(200).render('pagesViewAdmin', {user})
